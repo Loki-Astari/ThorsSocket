1be38e2fae623b6b7177ca1d476fe93fb1e31600
Loki Astari <Loki.Astari@gmail.com>
2024-06-17 19:15:38 -0700

    Define the SocketStream classes

--- src/ThorsSocket/SocketStream.h
+++ src/ThorsSocket/SocketStream.h
@@ -2,9 +2,26 @@
 #define THORSANVIL_THORSSOCKET_SOCKET_STREAM_H
 
 #include "ThorsSocketConfig.h"
+#include "Socket.h"
+#include <iostream>
 
 namespace ThorsAnvil::ThorsSocket
 {
+
+class SocketStream: public std::iostream
+{
+    class SocketStreamBuffer: std::basic_streambuf
+    {
+        Socket                  socket;
+        std::vector<char>       inputBuffer;
+        std::vector<char>       outputBuffer;
+        public:
+            SocketStreamBuffer(SocketInfo const& info);
+    };
+
+    public:
+        SocketStream(SocketInfo const& info);
+};
 
 }
 
