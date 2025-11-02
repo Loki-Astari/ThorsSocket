924a1e27751c9b0870db2db1179bd03727313d3c
Loki Astari <Loki.Astari@gmail.com>
2025-11-02 00:52:40 -0700

    Allow test to show debug logging information

---
+++ src/ThorsSocket/test/Logging.cpp
@@ -0,0 +1,79 @@
+#include "SerializeConfig.h"
+#include "gtest/gtest.h"
+#include "ThorsLogging/ThorsLogging.h"
+#include <iostream>
+#include <string>
+
+class LoggingEnvironment: public ::testing::Environment
+{
+    public:
+    ~LoggingEnvironment() override {}
+
+    // Override this to define how to set up the environment.
+    void SetUp() override
+    {
+        using namespace std::string_literals;
+
+        char* logging = getenv("THOR_LOG_LEVEL");
+        if (logging == nullptr)
+        {
+            std::cerr << "NO - THOR_LOG_LEVEL\n";
+        }
+        else
+        {
+            std::cerr << "Logging Level BEFORE: " << loguru::g_stderr_verbosity << "\n";
+            std::cerr << "THOR_LOG_LEVEL = >" << logging << "<\n";
+            int level = std::strtol(logging, nullptr, 10);
+            if (level > 0 && level <10)
+            {
+                loguru::g_stderr_verbosity = level;
+            }
+            else if ("FATAL"s == logging)
+            {
+                loguru::g_stderr_verbosity = loguru::Verbosity_FATAL;
+            }
+            else if ("ERROR"s == logging)
+            {
+                loguru::g_stderr_verbosity = loguru::Verbosity_ERROR;
+            }
+            else if ("WARNING"s == logging)
+            {
+                loguru::g_stderr_verbosity = loguru::Verbosity_WARNING;
+            }
+            else if ("INFO"s == logging)
+            {
+                loguru::g_stderr_verbosity = loguru::Verbosity_INFO;
+            }
+            else if ("DEBUG"s == logging)
+            {
+                loguru::g_stderr_verbosity = 6;
+            }
+            else if ("TRACE"s == logging)
+            {
+                loguru::g_stderr_verbosity = 8;
+            }
+            else if ("ALL"s == logging)
+            {
+                loguru::g_stderr_verbosity = 9;
+            }
+            std::cerr << "Logging Level AFTER: " << loguru::g_stderr_verbosity << "\n";
+        }
+    }
+
+    // Override this to define how to tear down the environment.
+    void TearDown() override {}
+};
+
+bool initLogging()
+{
+    std::cerr << "Logging Init\n";
+    ::testing::Environment* const foo_env = ::testing::AddGlobalTestEnvironment(new LoggingEnvironment);
+    ((void)foo_env);
+    return true;
+}
+
+#ifdef THOR_DISABLE_TEST_LOGGING
+bool logingInit = false;
+#else
+bool logingInit = initLogging();
+#endif
