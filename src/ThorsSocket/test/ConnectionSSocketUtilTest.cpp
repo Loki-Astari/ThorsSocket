5e1dd64a965344f647e9df855f3b11f2ceba0234
Loki Astari <Loki.Astari@gmail.com>
2026-03-30 20:49:48 -0700

    Fix

--- src/ThorsSocket/test/ConnectionSSocketUtilTest.cpp
+++ src/ThorsSocket/test/ConnectionSSocketUtilTest.cpp
@@ -364,11 +364,9 @@ TEST(ConnectionSSocketUtilTest, CertifcateAuthorityInfoDefaultConstruct)
         CertifcateAuthorityInfo     ca;
         ca.apply(reinterpret_cast<SSL_CTX*>(0x08));
     })
-/*
     .expectCallTA(SSL_CTX_set_default_verify_file).toReturn(1)
     .expectCallTA(SSL_CTX_set_default_verify_dir).toReturn(1)
     .expectCallTA(SSL_CTX_set_default_verify_store).toReturn(1)
-*/
     .run();
 }
 
